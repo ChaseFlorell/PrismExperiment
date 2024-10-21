513cb8b6a1d294998b500df0d3c9e0506196f1f6
Chase Florell <chase.florell@gmail.com>
2024-10-21 10:02:00 -0700

    [CF] checkpoint

--- submodules/Pep.Ioc/ContainerRegistry.cs
+++ submodules/Pep.Ioc/ContainerRegistry.cs
@@ -3,10 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Pep.Ioc;
 
-public class ContainerRegistry : IContainerRegistry
-{
-}
-
 public interface IContainerRegistry
 {
     IContainerRegistry RegisterInstance<TService>(TService implementation);
@@ -55,6 +51,138 @@ public class PepIocContainerExtension : IContainerExtension
     public PepIocContainerExtension(Rules rules)
     {
     }
+
+    /// <inheritdoc />
+    public T Resolve<T>(Type type)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public T Resolve<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public object Resolve(Type type, params (Type, object Instance)[] valueTuple)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerProvider CreateScope()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public bool IsRegistered<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry RegisterInstance<TService>(TService implementation)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry TryRegisterSingleton<TService, TImplementation>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry TryRegisterScoped<TService, TImplementation>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry Register<TService, TImplementation>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry TryRegister<TService, TImplementation>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry RegisterSingleton<TService, TImplementation>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry RegisterScoped<TService, TImplementation>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry RegisterInstance(object prismAppBuilder)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry Register<T>(Func<T> func)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry Register(Type type)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry Register<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry TryRegister<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry RegisterSingleton<TService>(Func<IContainerProvider, TService> func)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry RegisterScoped<TService>(Func<IContainerProvider, TService> func)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry RegisterManySingleton<TImplementation>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainerRegistry RegisterForNavigation<TPage, TViewModel>(string scopeName)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public IContainer GetContainer()
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public class ContainerResolutionException : Exception
@@ -70,13 +198,13 @@ public class PrismServiceProviderFactory : IServiceProviderFactory<IContainerExt
     /// <inheritdoc />
     public IContainerExtension CreateBuilder(IServiceCollection services)
     {
-        return TODO_IMPLEMENT_ME;
+        throw new NotImplementedException();
     }
 
     /// <inheritdoc />
     public IServiceProvider CreateServiceProvider(IContainerExtension containerBuilder)
     {
-        return TODO_IMPLEMENT_ME;
+        throw new NotImplementedException();
     }
 }
 
@@ -84,5 +212,6 @@ public static class ExceptionExtensions
 {
     public static Exception GetRootException(this Exception exception)
     {
+        throw new NotImplementedException();
     }
 }
